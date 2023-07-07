13881f9396b01fbd10ef571f9fa6ac0f54e331fc
로컬이 <125847524+EasyLocal@users.noreply.github.com>
2023-07-07 22:47:58 +0900

    Create DefaultImage.lua

---
+++ others/DefaultImage.lua
@@ -0,0 +1,10 @@
+local Images = {
+['info'] = 8942361465;
+['main'] = 4483362458;
+['localPlayer'] = 10653366793;
+['credits'] = 12647289920;
+['youtube image'] = 12647289920;
+['discord'] = 11633203664;
+}
+
+return Images
